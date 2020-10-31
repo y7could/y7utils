76f8a0778f7adc69e3028b16036e7e1f0497d922
mryang <719498808@qq.com>
2020-10-31 20:46:17 +0800

    新增查询方法

---
+++ src/ip/ipobject.go
@@ -0,0 +1,48 @@
+package ip
+
+import (
+	"github.com/ipipdotnet/ipdb-go"
+	"y7utils/src/ip/iptocity"
+)
+
+type ipObject struct {
+	Ipdbobj   *ipdb.City
+	Ip2region *iptocity.Iptocity
+}
+
+func NewIpObj(ip2regionpath string, ipdbpath string) *ipObject {
+	var ipObj ipObject
+	if ip2regionpath != "" {
+		ipObj.Ip2region, _ = iptocity.New(ip2regionpath)
+	}
+	if ipdbpath != "" {
+		ipObj.Ipdbobj, _ = ipdb.NewCity(ipdbpath)
+	}
+	return &ipObj
+}
+func (this *ipObject) finIp(ip string) (ret map[string]string) {
+	if this.Ip2region != nil {
+		ipinfo, err := this.Ip2region.MemorySearch(ip)
+		if err != nil {
+			panic(err)
+		}
+		ret["country"] = ipinfo.Country
+		ret["province"] = ipinfo.Province
+		ret["city"] = ipinfo.City
+		ret["isp"] = ipinfo.ISP
+		if ret["country"] != "" && ret["province"] != "" {
+			return ret
+		}
+	}
+	if this.Ipdbobj != nil {
+		ipdbinfo, err := this.Ipdbobj.FindMap(ip, "CN")
+		if err != nil {
+			panic(err)
+		}
+		ret["country"] = ipdbinfo["country_name"]
+		ret["province"] = ipdbinfo["region_name"]
+		ret["city"] = ipdbinfo["city_name"]
+		ret["isp"] = ipdbinfo["isp_domain"]
+	}
+	return
+}
